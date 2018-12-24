8f5a3205a0bcaa8abee0a5fff0fc85fa5f16555d
Kuvuklija <cerakuzz@mail.ru>
2018-12-24 22:18:08 +0300

    v2

---
+++ Filter/Filter/Infrastructure/CustomAuthAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web;
+
+
+namespace Filter.Infrastructure
+{
+    public class CustomAuthAttribute:AuthorizeAttribute
+    {
+        private bool localAllowed;
+
+        public CustomAuthAttribute(bool allowedParam) {
+            localAllowed = allowedParam;
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Request.IsLocal) {
+                return localAllowed;
+            }else {
+                return true;
+            }
+        }
+    }
+}

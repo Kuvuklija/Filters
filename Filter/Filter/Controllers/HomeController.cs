7d8a8857ce98c6a6d84410a3ffc7fe3e32de09a2
Kuvuklija <cerakuzz@mail.ru>
2018-12-24 22:31:29 +0300

    v4

--- Filter/Filter/Controllers/HomeController.cs
+++ Filter/Filter/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Filter.Infrastructure;
 
 namespace Filter.Controllers
 {
     public class HomeController : Controller
     {
         //passing Razor
+        [CustomAuth(false)]
         public string Index()
         {
             return "This is the Index action on the Home controller";

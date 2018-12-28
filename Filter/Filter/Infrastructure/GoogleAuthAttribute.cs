27fb9964d6d3325aec01b9abea75e69ec4595e80
Kuvuklija <cerakuzz@mail.ru>
2018-12-28 21:26:58 +0300

    authentication filter (google)

---
+++ Filter/Filter/Infrastructure/GoogleAuthAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Principal;
+using System.Web.Routing;
+using System.Web.Mvc.Filters;
+using System.Web.Mvc;
+
+namespace Filter.Infrastructure
+{
+    public class GoogleAuthAttribute : FilterAttribute, IAuthenticationFilter
+    {
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            IIdentity ident = filterContext.Principal.Identity;
+            if(!ident.IsAuthenticated || !ident.Name.EndsWith("google.com")) {
+                filterContext.Result = new HttpUnauthorizedResult();
+            }
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            if (filterContext.Result == null || filterContext.Result is HttpUnauthorizedResult) {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary {
+                {"controller", "GoogleAccount"},
+                {"action","Login"},
+                {"returnUrl",filterContext.HttpContext.Request.RawUrl}
+                });
+            }
+        }
+    }
+}
